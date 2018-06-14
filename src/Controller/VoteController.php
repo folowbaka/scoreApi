faa653079686a633baf9704162dc4b3db6134721
Folow <davidgalante77@gmail.com>
2018-06-14 22:46:45 +0200

    Maj du code

--- src/Controller/VoteController.php
+++ src/Controller/VoteController.php
@@ -2,11 +2,16 @@
 
 namespace App\Controller;
 
+use App\Modele\Vote;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use FOS\RestBundle\Controller\Annotations\Get;
 use FOS\RestBundle\Controller\Annotations\Post;
 use FOS\RestBundle\Controller\Annotations\Put;
+use FOS\RestBundle\View\View;
+use FOS\RestBundle\View\ViewHandler;
+use JMS\Serializer\SerializationContext;
+use Symfony\Bundle\FrameworkBundle\Templating\TemplateReference;
 
 class VoteController extends Controller
 {
@@ -15,20 +20,39 @@ class VoteController extends Controller
      */
     public function getVoteAction($slug)
     {
-        return $this->json([
-            'message' => 'Welcome to your new controller!',
-            'path' => 'src/Controller/VoteController.php',
-        ]);
+        $vote = $this->getDoctrine()
+            ->getRepository(\App\Entity\Vote::class)
+            ->find($slug);
+        $view = $this->view($vote, 200)
+            ->setTemplate("rating-bar")
+            ->setTemplateVar('vote');
+        return $this->handleView($view);
     }
     /**
      * @put("/vote/{slug}", name="put_vote")
      */
     public function putVoteAction($slug)
     {
-        return $this->json([
-            'message' => 'Welcome to your new controller!',
-            'path' => 'src/Controller/VoteController.php',
-        ]);
+        $vote = $this->getDoctrine()
+            ->getRepository(\App\Entity\Vote::class)
+            ->find($slug);
+        if ($vote == null)
+        {
+            $entityManager = $this->getDoctrine()->getManager();
+            $vote=new \App\Entity\Vote();
+            $vote->setPageSlug($slug);
+            $vote->setTotalValue(0);
+            $vote->setTotalVotes(0);
+            $entityManager->persist($vote);
+            $entityManager->flush();
+        }
+        $view = new View();
+        $view->setData($vote);
+        $context = new SerializationContext();
+        $context->setVersion('2.1');
+        $context->setGroups(array('data'));
+        $view->setSerializationContext($context);
+        return $this->viewHandler->handle($view);
     }
     /**
      * @post("/vote/{slug}/{scoreValue}", name="post_vote")
--- src/Modele/Vote.php
+++ src/Modele/Vote.php
@@ -12,36 +12,25 @@ namespace App\Modele;
 class Vote
 {
 
-    function rating_bar($pageSlug, $units='', $static='') {
+    public static function rating_bar($vote, $units='', $static='') {
 
         //set some variables
         $rating_unitwidth = 30;
-        $ip = $_SERVER['REMOTE_ADDR'];
         if (!$units) {$units = 5;}
         if (!$static) {$static = FALSE;}
-
+        $pageSlug=$vote->getPageSlug();
         // get votes, values, ips for the current rating bar
-        $query = DB::getInstance()->query("SELECT total_votes, total_value, used_ips FROM ratings WHERE page_id='".$page_id."'");
 
-        // insert the id in the DB if it doesn't exist already
-        // see: http://www.masugadesign.com/the-lab/scripts/unobtrusive-ajax-star-rating-bar/#comment-121
-        $count = $query->rowCount();
-        if ($count == 0) {
-            $req = "INSERT INTO `vote`.`ratings` (`vote_id` ,`page_id` ,`total_votes` ,`total_value` ,`used_ips` ) VALUES (NULL , '".$page_id."', '0', '0', NULL);";
-            $result = DB::getInstance()->exec($req);
-        }
 
-        $numbers = $query->fetch(PDO::FETCH_ASSOC);
+        if ($vote->getTotalVotes()<1) { $count = 0; }
+        else { $count=$vote->getTotalVotes(); }
 
-        if ($numbers['total_votes'] < 1) { $count = 0; }
-        else { $count=$numbers['total_votes']; }
-
-        $current_rating=$numbers['total_value']; //total number of rating added together and stored
+        $current_rating=$vote->getTotalValue(); //total number of rating added together and stored
         $tense=($count==1) ? "vote" : "votes"; //plural form votes/vote
 
         // determine whether the user has voted, so we know how to draw the ul/li
-        $req = DB::getInstance()->query("SELECT used_ips FROM ratings WHERE used_ips LIKE '%".$ip."%' AND page_id='".$page_id."'");
-        $voted = $req->rowCount();
+        //$req = DB::getInstance()->query("SELECT used_ips FROM ratings WHERE used_ips LIKE '%".$ip."%' AND page_id='".$page_id."'");
+        //$voted = $req->rowCount();
 
         // now draw the rating bar
 
@@ -62,8 +51,8 @@ class Vote
 
             $static_rater = array();
             $static_rater[] .= "\n".'<div class="ratingblock">';
-            $static_rater[] .= '<div id="unit_long'.$page_id.'">';
-            $static_rater[] .= '<ul id="unit_ul'.$page_id.'" class="unit-rating" style="width:'.$rating_unitwidth*$units.'px;">';
+            $static_rater[] .= '<div id="unit_long'.$pageSlug.'">';
+            $static_rater[] .= '<ul id="unit_ul'.$pageSlug.'" class="unit-rating" style="width:'.$rating_unitwidth*$units.'px;">';
             $static_rater[] .= '<li class="current-rating" style="width:'.$rating_width.'px;">Currently '.$rating2.'/'.$units.'</li>';
             $static_rater[] .= '</ul>';
             $static_rater[] .= '<p class="static"><strong> '.$rating1.'</strong>/'.$units.' ('.$count.' '.$tense.' ) <em>This is \'static\'.</em></p>';
@@ -76,13 +65,14 @@ class Vote
 
             $rater ='';
             $rater.='<div class="ratingblock">';
-            $rater.='<div id="unit_long'.$page_id.'">';
-            $rater.='<ul id="unit_ul'.$page_id.'" class="unit-rating" style="width:'.$rating_unitwidth*$units.'px;">';
+            $rater.='<div id="unit_long'.$pageSlug.'">';
+            $rater.='<ul id="unit_ul'.$pageSlug.'" class="unit-rating" style="width:'.$rating_unitwidth*$units.'px;">';
             $rater.='<li class="current-rating" style="width:'.$rating_width.'px;">Currently '.$rating2.'/'.$units.'</li>';
 
+            $voted=false;
             for ($ncount = 1; $ncount <= $units; $ncount++) { // loop from 1 to the number of units
                 if(!$voted) { // if the user hasn't yet voted, draw the voting stars
-                    $rater.='<li><a href="/vote/vote/db.php?j='.$ncount.'&amp;q='.$page_id.'&amp;t='.$ip.'&amp;c='.$units.'" title="'.$ncount.' sur '.$units.'" class="r'.$ncount.'-unit rater" rel="nofollow">'.$ncount.'</a></li>';
+                    $rater.='<li><a href="/vote/vote/db.php?j='.$ncount.'&amp;q='.$pageSlug.'&amp;c='.$units.'" title="'.$ncount.' sur '.$units.'" class="r'.$ncount.'-unit rater" rel="nofollow">'.$ncount.'</a></li>';
                 }
             }
 
